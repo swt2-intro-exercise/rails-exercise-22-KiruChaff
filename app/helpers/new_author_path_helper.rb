509a515dd6158ad8a99a7ad1e0481ad518470df8
KiruChaff <spreukiru@gmail.com>
2022-10-27 17:41:13 +0200

    authors new

---
+++ app/helpers/new_author_path_helper.rb
@@ -0,0 +1,2 @@
+module NewAuthorPathHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rails routes".
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # author resource
+  resources :authors
 
   # You can have the root of your site routed with "root"
   root 'home#index'
