8aa390249744e3a69b621c9499797fb5d1e95697
KiruChaff <spreukiru@gmail.com>
2022-11-03 16:46:05 +0100

    added editing

---
+++ spec/features/author/edit_author_spec copy.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "Edit author page", type: :feature do
+  it "an authors attributes should change after editing" do
+    # dont know how
+    # todo later!
+  end
+
+end
