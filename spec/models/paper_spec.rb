b3b3f81b02123b720b25f33503f508d9e41cdd58
KiruChaff <spreukiru@gmail.com>
2022-11-06 16:31:58 +0100

    added validation for papers

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "a paper must include a title, venue and an integer year" do 
+    paper1 = Paper.new({:title => "title", :venue => "venue", :year => 2022})
+    paper2 = Paper.new({                   :venue => "venue", :year => 2022})
+    paper3 = Paper.new({:title => "title",                    :year => 2022})
+    paper4 = Paper.new({:title => "title", :venue => "venue"               })
+    paper5 = Paper.new({:title => "title", :venue => "venue",  :year => "foo"})
+
+    expect(paper1).to be_valid
+    expect(paper2).to_not be_valid
+    expect(paper3).to_not be_valid
+    expect(paper4).to_not be_valid
+    expect(paper5).to_not be_valid
+  end
 end
