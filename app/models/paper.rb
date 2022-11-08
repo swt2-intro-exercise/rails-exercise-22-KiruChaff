58ceda5c65cc80fd922741b0f11b328460ac07aa
KiruChaff <spreukiru@gmail.com>
2022-11-08 13:04:50 +0100

    add test for query

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -2,4 +2,7 @@ class Paper < ApplicationRecord
     validates :title, :venue, :year, presence: true
     validates :year, numericality: {only_integer: true}
     has_and_belongs_to_many :authors
+    
+    scope :year,  -> (publishing_year) { where("year IS ?", publishing_year)}
+
 end
--- spec/views/papers/index.html.erb_spec.rb
+++ spec/views/papers/index.html.erb_spec.rb
@@ -23,4 +23,22 @@ RSpec.describe "papers/index", type: :view do
     assert_select cell_selector, text: Regexp.new("Venue".to_s), count: 2
     assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
   end
+
+  it "queries by year" do
+    paper = FactoryBot.create :paper
+    visit edit_paper_path(paper)
+    new_venue = paper.venue + '_updated'
+    page.fill_in 'paper[venue]', with: new_venue
+    find('input[type="submit"]').click
+    expect(paper.reload.venue).to eq(new_venue)
+
+
+    # paper = FactoryBot.create :paper
+    # vist edit_paper_path(paper)
+    # year_zero = '0'
+    # page.fill_in 'paper[year]', with: year_zero
+    # find('input[type="submit"]').click
+    # expect(Paper.year(year_zero).include? paper).to eq(true)
+
+  end
 end
